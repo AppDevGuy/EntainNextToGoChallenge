74a3dedeba69c2b0e9a30b63effd9c5774921172
Sean Smith <seaniosdeveloper@gmail.com>
2024-02-27 19:27:03 +0800

    Base App

---
+++ EntainNextToGo/EntainNextToGo/EntainNextToGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EntainNextToGoApp.swift
+//  EntainNextToGo
+//
+//  Created by Sean Smith on 27/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct EntainNextToGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
